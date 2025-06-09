e1700d2b22e31b9574bbcd65573aec2c826f5d08
PauloHenrique00-tech <saraiva_ph@hotmail.com>
2025-06-09 11:04:40 -0300

    Create dados.js

---
+++ modulos-esm/dados.js
@@ -0,0 +1,18 @@
+'use strict';
+// dados.js
+
+const pessoa = {
+    nome: 'Ian Gillan',
+    idade: 78,
+    cidade: 'São Paulo'
+};
+
+const livro = {
+    titulo: 'O Senhor dos Anéis',
+    volume: 'As Duas Torres',
+    ano: 1954
+};
+
+const alunos = ['Pica Pau', 'Zé Colmeia', 'Naruto', 'Luffy'];
+
+export { pessoa, livro, alunos }
