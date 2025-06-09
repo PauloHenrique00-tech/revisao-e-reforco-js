dc7c14f953f67f2052499a1fe884f96ab7d94653
PauloHenrique00-tech <saraiva_ph@hotmail.com>
2025-06-09 11:43:34 -0300

    Cria funcoes.js dentro de modulos esm e aplica

---
+++ modulos-esm/funcoes.js
@@ -0,0 +1,16 @@
+'use strict'
+
+const nomeMaiusculo = (nome) => {
+    return nome.toUpperCase();
+}
+console.log(nomeMaiusculo("nome"));
+
+const nomeMinusculo = (nome) => {
+    return nome.toLowerCase();
+}
+console.log(nomeMinusculo("afonso"));
+
+const valorFormatado = (valor) => {
+    return valor = valor.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL'});
+}
+console.log(valorFormatado(1000));
