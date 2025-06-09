eed66b573bcfc49d5c4d0cc8ba636924ffd09777
PauloHenrique00-tech <saraiva_ph@hotmail.com>
2025-06-09 09:34:06 -0300

    Cria e exporta módulo default

---
+++ modulos.esm/alunos.js
@@ -0,0 +1,6 @@
+'use strict';
+
+const alunos = ["Ozzy", "Dio", "David", "Eddie", "Jon"];
+
+// Exportando como deafult (padrão), útil para recurso único (não precisa de objeto)
+export default alunos;
