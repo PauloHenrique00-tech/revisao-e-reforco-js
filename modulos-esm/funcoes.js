7e4a0ab7a9783215d226f70b01e9c913d934d57d
PauloHenrique00-tech <saraiva_ph@hotmail.com>
2025-06-10 11:06:08 -0300

    Adiciona função classificar

--- modulos-esm/funcoes.js
+++ modulos-esm/funcoes.js
@@ -14,4 +14,7 @@ export const valorFormatado = (valor) => {
     return valor = valor.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL'});
 }
 
-
+/* Usamos o rest operator para obter uma lista de parâmetros, não importando a quantidade */
+export function classificar(...texto){
+    return texto.sort();
+}
